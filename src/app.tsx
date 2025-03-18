0d1fb5b8da445f54d9f82e584f3a17bc98f05826
Ardian Shala <ardian.sh@outlook.de>
2025-03-18 09:19:04 +0100

    upgraded packages

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-export const App = (): JSX.Element => {
+export const App = (): React.JSX.Element => {
 	const [count, setCount] = useState(0);
 
 	return (
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	server: {
 		port: 3333,
 	},
-	plugins: [react(), tsconfigPaths()],
+	plugins: [react()],
 });
